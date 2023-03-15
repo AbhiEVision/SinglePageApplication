260ea012d62208baf2d4a39f15daf69988148868
Private <abhi.d@simformsolutions.com>
2023-03-15 18:46:09 +0530

    Add : Script.js Basic onclick function for navbar

---
+++ Script/Script.js
@@ -0,0 +1,15 @@
+function GoHome(){
+    location.href = "#home-section"
+}
+
+function GoServices(){
+    location.href = "#Services"
+}
+
+function GoSlider(){
+    location.href = "#Slider"
+}
+
+function GoContactUs(){
+    location.href = "#ContactUs"
+}
