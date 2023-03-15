ad931192392599e900541d67f676b5c0bfe7bebc
Private <abhi.d@simformsolutions.com>
2023-03-15 18:48:34 +0530

    Add : Typing.js Header text are implemented using typedJS

---
+++ Script/Typing.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#typing-here', {
+    strings: ["Game Developer.", ".NET Developer."],
+    typeSpeed: 100,
+    loop : true,
+    backSpeed: 100,
+    backDelay : 1000.
+  });
